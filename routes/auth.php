e33ecfcaa768670efa4adb74e7d2fe6bef437c2f
Callum Macalast <callummacalast@callums-mbp-2.home>
2022-05-23 18:45:41 +0100

    Added register back

--- routes/auth.php
+++ routes/auth.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
-    Route::get('register', function () {
-        return redirect('/');
     });
 
     Route::post('register', [RegisteredUserController::class, 'store']);
