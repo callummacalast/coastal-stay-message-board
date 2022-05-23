0e1efc141a2b057aec03b8c15ece882ae207022e
Callum Macalast <callummacalast@callums-mbp-2.home>
2022-05-23 18:22:38 +0100

    db again

--- config/database.php
+++ config/database.php
@@ -1,8 +1,13 @@
 <?php
-$CLEARDB_DATABASE_URL = parse_url('mysql://b01afa1e080637:c22a1c89@us-cdbr-east-05.cleardb.net/heroku_5c54f95a90c7670?reconnect=true');
+
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"] ?? null;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -47,11 +52,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
